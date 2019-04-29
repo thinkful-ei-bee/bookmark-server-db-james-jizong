e6b4c00c3d5697b5a06b58453fc3b078fbf3281d
JizongL <2015rpro@gmail.com>
2019-04-29 00:06:33 -0400

    debug apitoken

--- src/validationHandler.js
+++ src/validationHandler.js
@@ -3,6 +3,7 @@ const logger = require('./logger')
 const validateBearerToken = function(req,res,next){
   const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
+  console.log(authToken,'authtoken',apiToken,'apitoken')
   if(!authToken ||authToken.split(' ')[1]!==apiToken){
     logger.error(`Unauthorized request to path: ${req.path}`);
     return res.status(401).json({error:'Unautherized request'})
